8bd9668e0405a964222ad4d52de7f15780d992e5
EmanSamnani06 <emansamnani@gmail.com>
2024-03-16 23:50:49 +0500

    Finding the smallest and greatest number from a List

---
+++ Question#4.dart
@@ -0,0 +1,7 @@
+void main() {
+  List numbers = [9, 5, 2, 6];
+  print("Original List: $numbers");
+  numbers.sort();
+  print("Smallest Number: ${numbers.first}");
+  print("Greatest Number: ${numbers.last}");
+}
