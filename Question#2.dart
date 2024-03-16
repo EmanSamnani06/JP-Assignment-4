863d3d5be6f4a50052834823947c763499ab228e
EmanSamnani06 <emansamnani@gmail.com>
2024-03-16 23:45:07 +0500

    Using add method of List datatype

---
+++ Question#2.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<String> days = [];
+  //Adding Names of 7days
+  days.add("Sunday");
+  days.add("Monday");
+  days.add("Tuesday");
+  days.add("Wednesday");
+  days.add("Thursday");
+  days.add("Friday");
+  days.add("Saturday");
+  //Printing the names of 7days
+  print(days);
+}
